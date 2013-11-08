249ca7333ced1e65b005a0f6968d38b0f4eaaaee
defra91 <luca.defranceschi.91@gmail.com>
2013-11-08 22:32:28 +0100

    Aggiunta la javadoc al progetto

--- Rimbalzi/src/Rimbalzi/gui/Ball.java
+++ Rimbalzi/src/Rimbalzi/gui/Ball.java
@@ -6,23 +6,59 @@ import java.awt.geom.*;
 import java.lang.Math;
 
 
-
+/** La classe Ball descrive una pallina che verr‡ visualizzata nello schermo come un'ellisse in due dimensione. Essa estende appunto il concetto di ellisse con tutte le sue propriet‡ geometriche. Possiede inoltre un colore, una massa
+ * espressa in kg, le sue componenti orizzontale e verticale della velocit‡ e il tempo di volo (nel caso in cui ci sia gravit‡ nel sistema).
+ * @author Luca De Franceschi
+ * @version 1.0, Ven 8 Novembre 2013
+ */
 public class Ball extends Ellipse2D.Double {
+	/** Descrive la posizione in pixel nell'asse x della finestra. */
 	private double posX = 0;
-	private double posY = 0;
-	private double velX = 0;	// velocit√† espressa in m/s
-	private double velY = 0;	// velocit√† espressa in m/s
-	private double xSize = 0;
-	private double ySize = 0;
-	private double weight = 0;		// massa espressa in kg
-	private Color borderColor;
-	private Color backgroundColor;
-	int time = 0;
 	
+	/** Descrive la posizione in pixel nell'asse y della finestra. */
+	private double posY = 0;
+	
+	/** Esprime in metri al secondo la componente orizzontale della velocit‡. */
+	private double velX = 0;
+	
+	/** Esprime in metri al secondo la componete verticale della velocit‡. */
+	private double velY = 0;	
+	
+	/** Esprime la larghezza in pixel dell'ellisse.*/
+	private double xSize = 0;
+	
+	/** Esprime l'altezza in pixel dell'ellisse.*/
+	private double ySize = 0;
+	
+	/** Esprime la massa della pallina espressa in kilogrammi */
+	private double weight = 0;
+	
+	/** Rappresenta il colore del bordo dell'ellisse. */
+	private Color borderColor;
+	
+	/** Rappresenta il colore di sfondo dell'ellisse. */
+	private Color backgroundColor;
+	
+	/** Rappresenta il tempo espresso in secondi in cui la pallina Ë in aria. */
+	double time = 0;
+	
+	/** Costruttore di default della pallina con tutti i campi dati settati a zero e colore impostato a nero. */
 	public Ball() {
 		borderColor = new Color(0,0,0);
 		backgroundColor = new Color(0,0,0);
 	}
+	
+	/** Costruttore parametrico della classe.
+	 * @param x	Indica la posizione x.
+	 * @param y Indica la posizione y.
+	 * @param vx Indica la velocit‡ iniziale orizzontale.
+	 * @param vy Indica la velocit‡ iniziale verticale.
+	 * @param xs Indica la larghezza della pallina.
+	 * @param ys Indica l'altezza della pallina.
+	 * @param w Indica la massa della pallina.
+	 * @param b1 Indica il colore del bordo della pallina.
+	 * @param b2 Indica il colore dello sfondo della pallina.
+	 */
 	public Ball(double x, double y, double vx, double vy, double xs, double ys, double w, Color b1, Color b2) {
 		super(x,y,xs,ys);
 		posX = x; posY = y;
@@ -32,30 +68,104 @@ public class Ball extends Ellipse2D.Double {
 		borderColor = b1; backgroundColor = b2;
 	}
 	
-	/* definizione dei metodi setter */
+	/** Imposta la posizione x della pallina espressa in pixel.
+	 * @param d Posizione x della pallina espressa in pixel.
+	 */
 	void setPosX(double d) { posX = d; }
+	
+	/** Imposta la posizione y della pallina espressa in pixel
+	 * @param d Posizione y della pallina espressa in pixel.
+	 */
 	void setPosY(double d) { posY = d; }
+	
+	/** Imposta la velocit‡ iniziale orizzontale della pallina espressa in metri al secondo.
+	 * @param d Velocit‡ x della pallina espressa in metri al secondo.
+	 */
 	void setVelX(double d) { velX = d; }
+	
+	/** Imposta la velocit‡ iniziale verticale della pallina espressa in metri al secondo.
+	 * @param d Velocit‡ y della pallina espressa in metri al secondo.
+	 */
 	void setVelY(double d) { velY = d; }
+	
+	/** Imposta la larghezza in pixel della pallina.
+	 * @param d Larghezza in pixel della pallina.
+	 */
 	void setXSize(double d) { xSize = d; }
+	
+	/** Imposta l'altezza in pixel della pallina.
+	 * @param d Altezza in pixel della pallina.
+	 */
 	void setYSize(double d) { ySize = d; }
+	
+	/** Imposta la massa della pallina espressa in kilogrammi.
+	 * @param d Massa della pallina in kg.
+	 */
 	void setWeight(double d) { weight = d; }
+	
+	/** Imposta il colore del bordo della pallina.
+	 * @param c Colore del bordo.
+	 */
 	void setBorderColor(Color c) { borderColor = c; }
+	
+	/** Imposta il colore dello sfondo della pallina.
+	 * @param c Colore dello sfondo.
+	 */
 	void setBackgroundColor(Color c) { backgroundColor = c; }
 	
-	/* definizione dei metodi getter */
+	/** Restituisce la posizione x della pallina in pixel.
+	 * @return posizione x della pallina.
+	 */
 	double getPosX() { return posX; }
+	
+	/** Restituisce la posizione y della pallina in pixel.
+	 * @return posizione y della pallina.
+	 */
 	double getPosY() { return posY; }
+	
+	/** Restituisce la velocit‡ x della pallina in metri al secondo.
+	 * @return velocit‡ x.
+	 */
 	double getVelX() { return velX; }
+	
+	/** Restituisce la velocit‡ y della pallina in metri al secondo.
+	 * @return velocit‡ y.
+	 */
 	double getVelY() { return velY; }
+	
+	/** Restituisce la larghezza della pallina in pixel.
+	 * @return larghezza della pallina.
+	 */
 	double getXSize() { return xSize; }
+	
+	/** Restituisce l'altezza della pallina in pixel.
+	 * @return altezza della pallina.
+	 */
 	double getYSize() { return ySize; }
+	
+	/** Restituisce la massa della pallina in kg.
+	 * @return massa della pallina.
+	 */
 	double getWeight() { return weight; }
+	
+	/** Restituisce il colore del bordo della pallina.
+	 * @return colore bordo.
+	 */
 	Color getBorderColor() { return borderColor; }
+	
+	/** Restituisce il colore dello sfondo della pallina.
+	 * @return colore sfondo.
+	 */
 	Color getBackgroundColor() { return backgroundColor; }
 	
+	/** Restituisce il centro della pallina sotto forma di punto bidimensionale.
+	 * @return centro della pallina.
+	 */
 	Point2D.Double getCenter() { return new Point2D.Double(posX+xSize/2,posY+ySize/2); }
 	
+	/** Si occupa di far muovere la pallina nel pannello secondo le leggi fisiche del moto parabolico uniformemente accelerato.
+	 * @param bounds pannello di riferimento sui cui si muove la pallina espresso come rettangolo bidimensionale.
+	 */
 	public void move(Rectangle2D bounds) {
 		double x = posX + velX*(time/100);
 		double y = posY - (velY*(time/100) - (0.5*9.81*Math.pow((time/100),2)));
--- Rimbalzi/src/Rimbalzi/gui/MainFrame.java
+++ Rimbalzi/src/Rimbalzi/gui/MainFrame.java
@@ -7,13 +7,34 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+/** Questa classe rappresenta la finestra principale della GUI del progetto. Contiene il pannello dove si muovono le palline più i vari bottoni per interagire su di esse.
+ * @author Luca De Franceschi
+ * @version 1.0, Ven 8 Novembre 2013
+ */
 public class MainFrame extends JFrame {
+	/** Riferimento al pannello (e quindi all'ambiente) contenuto in questo frame */
 	private Environment e;
+	
+	/** Pulsante per iniziare la simulazione. */
 	private JButton start;
+	
+	/** Pulsante per interrompere la simulazione. */
 	private JButton stop;
+	
+	/** Pulsante per riesumare la simulazione. */
 	private JButton resume;
+	
+	/** Riferimento al thread che si occupa di far muovere le palline. */
 	Thread t;
 	
+	/** Costruttore parametrico della classe.
+	 * @param x Posizione x della finestra in pixel.
+	 * @param y Posizione y della finestra in pixel.
+	 * @param w Larghezza della finestra in pixel.
+	 * @param h Altezza della finestra in pixel.
+	 * @param bg Colore di sfondo del pannello.
+	 * @param title Titolo della finestra
+	 */
 	public MainFrame(int x, int y, int w, int h, Color bg, String title) {
 		super(title);
 		setSize(w,h);
