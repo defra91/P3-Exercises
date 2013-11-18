cbf2023d8bac9a1aabbadbe93410b21a0c5eb316
defra91 <luca.defranceschi.91@gmail.com>
2013-11-18 12:57:27 +0100

    Aggiunto un nuovo esempio e modificato il programma AB

--- AB/src/Syncronizer.java
+++ AB/src/Syncronizer.java
@@ -1,40 +1,39 @@
 class Process extends Thread {
 	private String name;
 	private int num;
-	private static int n = 0;
-	private Syncronizer s;
-	public void run() {
-		while (true) {
-			int l = s.getLock();
-			if (l == num) {
-				s.lockIncrement();
-				System.out.println(name);
-				if (s.getLock() > s.getNProcesses()) { s.lockReset(); }
-			}
-		}
+	Syncronizer rif;
+	public Process(String s, Syncronizer o, int n) {
+		name = s;
+		rif = o;
+		num = n;
 	}
 	
-	public Process(String x, Syncronizer y) {
-		name = x;
-		n++;
-		num = n;
-		s = y;
+	public void run() {
+		synchronized(rif) {
+			while (num != rif.getTurn()) {
+				try {
+					rif.wait();
+				} catch (InterruptedException e) {}
+			}
+			System.out.println(name);
+			if (num == rif.getNProcesses()) { rif.resetTurn(); }
+			else rif.incrementTurn();
+			rif.notify();
+		}
 	}
 }
 
 public class Syncronizer {
-	private int lock = 1;
-	private int nProcesses;
-	public Syncronizer(int n) {
-		nProcesses = n;
-	}
-	public int getLock() { return lock; }
+	int turn = 1;
+	int nProcesses;
+	public Syncronizer(int n) { nProcesses = n; }
+	public int getTurn() { return turn; }
 	public int getNProcesses() { return nProcesses; }
-	public void lockIncrement() { lock++; }
-	public void lockReset() { lock = 1; }
+	public void incrementTurn() { turn++; }
+	public void resetTurn() { turn = 1; }
 	public static void main(String[] args) {
-		Syncronizer s = new Syncronizer(4);
-		Thread[] t = {new Process("A",s), new Process("B",s), new Process("C",s), new Process("D",s)};
+		Syncronizer lock = new Syncronizer(4);
+		Thread[] t = {new Process("A",lock,1), new Process("B",lock,2), new Process("C",lock,3), new Process("D",lock,4)};
 		for (int i=0; i<t.length; i++) {
 			t[i].start();
 		}
